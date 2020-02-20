c8df7faaf93b292de40f74a141f50aa6107e657d
bartlomiej94 <47932951+bartlomiej94@users.noreply.github.com>
2020-02-20 21:39:17 +0000

    Add files via upload

---
+++ TunnelChangeUrl.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import Button from "./../../common/button";
+import { AsyncStorage } from "react-native";
+import TunnelUrlWindow from "./../../common/tunnelUrlWindow";
+
+export default TunnelChangeUrl = () => {
+  const [newTunnelUrlLocal, setNewTunnelUrlLocal] = useState("");
+  const [currentTunnelUrlLocal, setCurrentTunnelUrlLocal] = useState("");
+  const [newTunnelUrlOutbound, setNewTunnelUrlOutbound] = useState("");
+  const [currentTunnelUrlOutbound, setCurrentTunnelUrlOutbound] = useState("");
+
+  const AS_getCurrentTunnelUrlLocal = async () => {
+    try {
+      const key = "@Fx_tunnelUrlLocal";
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        setNewTunnelUrlLocal(value);
+      }
+    } catch (error) {
+      console.log(`Error: Couldn't get local data ${key}. ${error}`);
+    }
+  };
+
+  const AS_getCurrentTunnelUrlOutbound = async () => {
+    try {
+      const key = "@Fx_tunnelUrlOutbound";
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        setNewTunnelUrlOutbound(value);
+      }
+    } catch (error) {
+      console.log(`Error: Couldn't get local data ${key}. ${error}`);
+    }
+  };
+
+  const handleTunnelUrlChange = (e, isLocal) => {
+    let value = e.nativeEvent.text.trim();
+    isLocal
+      ? setCurrentTunnelUrlLocal(value)
+      : setCurrentTunnelUrlOutbound(value);
+  };
+
+  useEffect(() => {
+    AS_getCurrentTunnelUrlLocal();
+    AS_getCurrentTunnelUrlOutbound();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <TunnelUrlWindow
+        handleTunnelUrlChange={handleTunnelUrlChange}
+        isLocal={true}
+        currentTunnelUrl={currentTunnelUrlLocal}
+        newTunnelUrl={newTunnelUrlLocal}
+        setNewTunnelUrl={setNewTunnelUrlLocal}
+        setCurrentTunnelUrl={setCurrentTunnelUrlLocal}
+        label="Local"
+        info="Used by the other trader to access your data."
+      />
+      <TunnelUrlWindow
+        handleTunnelUrlChange={handleTunnelUrlChange}
+        isLocal={false}
+        currentTunnelUrl={currentTunnelUrlOutbound}
+        newTunnelUrl={newTunnelUrlOutbound}
+        setNewTunnelUrl={setNewTunnelUrlOutbound}
+        setCurrentTunnelUrl={setCurrentTunnelUrlOutbound}
+        label="Outbound"
+        info="Used to access other trader's data."
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 40,
+    backgroundColor: "#004d00"
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+    opacity: 0.6,
+    textAlign: "center"
+  },
+  textSm: {
+    fontSize: 16,
+    marginTop: 20,
+    fontWeight: "bold",
+    color: "#fff",
+    opacity: 0.6,
+    textAlign: "center"
+  },
+  inputWrap: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30
+  },
+  input: {
+    width: 100,
+    height: 40,
+    borderColor: "#7a42f4",
+    borderWidth: 1,
+    color: "#fff"
+  },
+  noMarginTop: {
+    marginTop: 0
+  }
+});
---
+++ tunnelUrlWindow.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { AsyncStorage } from "react-native";
+import Button from "./button";
+
+export default TunnelUrlWindow = ({
+  handleTunnelUrlChange,
+  isLocal,
+  newTunnelUrl,
+  currentTunnelUrl,
+  setNewTunnelUrl,
+  label,
+  info
+}) => {
+  const AS_setNewTunnelUrl = async isLocal => {
+    const key = isLocal ? "@Fx_tunnelUrlLocal" : "@Fx_tunnelUrlOutbound";
+    try {
+      console.log(currentTunnelUrl);
+      await AsyncStorage.setItem(key, currentTunnelUrl);
+      setNewTunnelUrl(currentTunnelUrl);
+    } catch (error) {
+      console.log(`Error: Couldn't set local data ${key}. ${error}`);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>{label} URL</Text>
+      <Text style={styles.textXs}>{info}</Text>
+      <Text style={styles.textSm}>CURRENT: http://{newTunnelUrl}.ngrok.io</Text>
+      <View style={styles.inputWrap}>
+        <Text style={{ ...styles.textSm, ...styles.noMarginTop }}>
+          NEW: http://
+        </Text>
+        <TextInput
+          onChange={e => handleTunnelUrlChange(e, isLocal)}
+          style={styles.input}
+        ></TextInput>
+        <Text style={{ ...styles.textSm, ...styles.noMarginTop }}>
+          .ngrok.io
+        </Text>
+      </View>
+      <Button
+        onPress={() => AS_setNewTunnelUrl(isLocal)}
+        label="Update"
+        color="#66ff33"
+      ></Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    borderColor: "#000",
+    borderRadius: 12,
+    width: "85%",
+    height: 280,
+    backgroundColor: "#fff",
+    marginBottom: 15,
+    marginTop: 15
+  },
+  header: {
+    fontSize: 22,
+    paddingTop: 10,
+    fontWeight: "bold",
+    color: "#000",
+    opacity: 0.6,
+    textAlign: "center"
+  },
+  textXs: {
+    fontSize: 10,
+    marginTop: 5,
+    fontWeight: "bold",
+    color: "#000",
+    opacity: 0.6,
+    textAlign: "center"
+  },
+  textSm: {
+    fontSize: 16,
+    marginTop: 20,
+    fontWeight: "bold",
+    color: "#000",
+    opacity: 0.6,
+    textAlign: "center"
+  },
+  inputWrap: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30
+  },
+  input: {
+    width: 100,
+    height: 40,
+    borderColor: "#7a42f4",
+    borderWidth: 1,
+    color: "#000"
+  },
+  noMarginTop: {
+    marginTop: 0
+  }
+});
